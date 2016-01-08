f34dded2c53e5f1bd9e837515ee0ddcd3f792805
Cerrato Renaud <renaud.cerrato@gmail.com>
2016-01-08 07:52:49 -0500

    added contract on setScrimAlpha

--- library/src/main/java/com/mypopsy/widget/SlidingUpPaneLayout.java
+++ library/src/main/java/com/mypopsy/widget/SlidingUpPaneLayout.java
@@ -11,6 +11,7 @@ import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.annotation.ColorInt;
 import android.support.annotation.DrawableRes;
+import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.MotionEventCompat;
@@ -249,7 +250,7 @@ public class SlidingUpPaneLayout extends ViewGroup {
         }
     }
 
-    private void setScrimAlpha(int alpha) {
+    private void setScrimAlpha(@IntRange(from = 0, to = 255) int alpha) {
         if (alpha != mScrimAlpha) {
             mScrimAlpha = alpha;
             ViewCompat.postInvalidateOnAnimation(this);
