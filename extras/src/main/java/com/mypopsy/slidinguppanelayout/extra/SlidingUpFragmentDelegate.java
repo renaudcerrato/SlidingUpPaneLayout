fa9ff29befb4ba782ab8e7a4b9b63260b65a1cf5
Cerrato Renaud <renaud.cerrato@gmail.com>
2016-01-12 13:45:43 -0600

    fixed: now remove view on destroyview

--- extras/src/main/java/com/mypopsy/slidinguppanelayout/extra/SlidingUpFragmentDelegate.java
+++ extras/src/main/java/com/mypopsy/slidinguppanelayout/extra/SlidingUpFragmentDelegate.java
@@ -18,48 +18,49 @@ import android.view.View;
 import com.mypopsy.widget.SlidingUpPaneLayout;
 import com.mypopsy.widget.SlidingUpPaneLayout.State;
 
+import java.io.Serializable;
+
 import static com.mypopsy.widget.SlidingUpPaneLayout.State.EXPANDED;
-import static com.mypopsy.widget.SlidingUpPaneLayout.State.HIDDEN;
 
 
-public final class SlidingUpFragmentDelegate extends SlidingUpPaneLayout.SimplePanelSlideListener{
+public final class SlidingUpFragmentDelegate extends SlidingUpPaneLayout.SimplePanelSlideListener
+        implements SlidingUpFragmentInterface {
 
     private static final String TAG = SlidingUpFragmentDelegate.class.getSimpleName();
     private static final boolean DEBUG = true;
 
     private static final String SAVED_SHOWN_AS_SLIDING_FRAGMENT = "slidinguppanel:shownAsSliding";
-    private static final String SAVED_BACK_STACK_ID = "slidinguppanel:backStackId";
+    private static final String SAVED_BACK_STACK_ID = "slidinguppanel:mBackStackId";
     private static final String SAVED_SLIDING_PANE_LAYOUT_ID = "slidinguppanel:layoutId";
     private static final String ARG_PANEL_STATE = "slidinguppanel:state";
 
     @IdRes
-    private int slidingPaneLayoutId = View.NO_ID;
-    private SlidingUpPaneLayout slidingUpLayout;
-    private boolean showAsSlidingFragment = true;
-    private int backStackId = -1;
-    private boolean viewDestroyed;
+    private int mSlidingPaneLayoutId = View.NO_ID;
+    @Nullable
+    private SlidingUpPaneLayout mSlidingUpLayout;
+    private boolean mShowSliding = true;
+    private int mBackStackId = -1;
+    private boolean mViewDestroyed;
+    private Fragment mFragment;
 
-    private Fragment fragment;
 
     public static SlidingUpFragmentDelegate create(Fragment fragment) {
         return new SlidingUpFragmentDelegate(fragment);
     }
 
     private SlidingUpFragmentDelegate(Fragment fragment) {
-        this.fragment = fragment;
+        this.mFragment = fragment;
     }
 
     /**
-     * DialogFragment-like show() method for displaying this the associated sheet fragment
-     *
-     * @param manager FragmentManager instance
-     * @param slidingPaneLayoutId Resource ID of the {@link SlidingUpPaneLayout}
+     * {@inheritDoc}
      */
+    @Override
     public void show(FragmentManager manager, @IdRes int slidingPaneLayoutId, State state) {
         if(DEBUG) Log.d(TAG, "-----show("+state+")");
-        this.slidingPaneLayoutId = slidingPaneLayoutId;
+        this.mSlidingPaneLayoutId = slidingPaneLayoutId;
         manager.beginTransaction()
-                .add(setState(fragment, state), "slidingfragment-"+slidingPaneLayoutId)
+                .add(putArg(mFragment, ARG_PANEL_STATE, state), "slidingfragment-"+slidingPaneLayoutId)
                 .commit();
     }
 
@@ -68,19 +69,16 @@ public final class SlidingUpFragmentDelegate extends SlidingUpPaneLayout.SimpleP
     }
 
     /**
-     * DialogFragment-like show() method for displaying this the associated sheet fragment
-     *
-     * @param transaction FragmentTransaction instance
-     * @param slidingPaneLayoutId Resource ID of the {@link SlidingUpPaneLayout}
-     * @return the back stack ID of the fragment after the transaction is committed.
+     * {@inheritDoc}
      */
+    @Override
     public int show(FragmentTransaction transaction, @IdRes int slidingPaneLayoutId, State state) {
         if(DEBUG) Log.d(TAG, "-----show("+state+")");
-        this.slidingPaneLayoutId = slidingPaneLayoutId;
+        this.mSlidingPaneLayoutId = slidingPaneLayoutId;
 
-        transaction.add(setState(fragment, state), "slidingfragment-" + slidingPaneLayoutId);
-        backStackId = transaction.commit();
-        return backStackId;
+        transaction.add(putArg(mFragment, ARG_PANEL_STATE, state), "slidingfragment-" + slidingPaneLayoutId);
+        mBackStackId = transaction.commit();
+        return mBackStackId;
     }
 
     public int show(FragmentTransaction transaction, @IdRes int slidingPaneLayoutId) {
@@ -88,26 +86,29 @@ public final class SlidingUpFragmentDelegate extends SlidingUpPaneLayout.SimpleP
     }
 
     /**
-     * Dismiss the fragment and it's bottom sheet. If the fragment was added to the back stack, all
-     * back stack state up to and including this entry will be popped. Otherwise, a new transaction
-     * will be committed to remove this fragment.
+     * {@inheritDoc}
      */
+    @Override
     public void dismiss() {
         dismissInternal(false);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void dismissAllowingStateLoss() {
         dismissInternal(true);
     }
 
     private void dismissInternal(boolean allowStateLoss) {
         if(DEBUG) Log.d(TAG, "-----dismiss("+allowStateLoss+")");
-        if (backStackId >= 0) {
-            fragment.getFragmentManager().popBackStack(backStackId, FragmentManager.POP_BACK_STACK_INCLUSIVE);
-            backStackId = -1;
+        if (mBackStackId >= 0) {
+            mFragment.getFragmentManager().popBackStack(mBackStackId, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+            mBackStackId = -1;
         } else {
-            FragmentTransaction ft = fragment.getFragmentManager().beginTransaction();
-            ft.remove(fragment);
+            FragmentTransaction ft = mFragment.getFragmentManager().beginTransaction();
+            ft.remove(mFragment);
             if (allowStateLoss) {
                 ft.commitAllowingStateLoss();
             } else {
@@ -118,132 +119,129 @@ public final class SlidingUpFragmentDelegate extends SlidingUpPaneLayout.SimpleP
 
     public void onCreate(@Nullable Bundle savedInstanceState) {
         if(DEBUG) Log.d(TAG, "-----onCreate()");
-        showAsSlidingFragment = AccessFragmentInternals.getContainerId(fragment) == 0;
+        mShowSliding = AccessFragmentInternals.getContainerId(mFragment) == 0;
 
         if (savedInstanceState != null) {
-            showAsSlidingFragment = savedInstanceState.getBoolean(SAVED_SHOWN_AS_SLIDING_FRAGMENT, showAsSlidingFragment);
-            backStackId = savedInstanceState.getInt(SAVED_BACK_STACK_ID, -1);
-            slidingPaneLayoutId = savedInstanceState.getInt(SAVED_SLIDING_PANE_LAYOUT_ID, View.NO_ID);
+            mShowSliding = savedInstanceState.getBoolean(SAVED_SHOWN_AS_SLIDING_FRAGMENT, mShowSliding);
+            mBackStackId = savedInstanceState.getInt(SAVED_BACK_STACK_ID, -1);
+            mSlidingPaneLayoutId = savedInstanceState.getInt(SAVED_SLIDING_PANE_LAYOUT_ID, View.NO_ID);
         }
     }
 
-    /**
-     * Retrieves the appropriate layout inflater, either the sheet's or the view's super container. Note that you should
-     * handle the result of this in your getLayoutInflater method.
-     *
-     * @param savedInstanceState Instance state, here to match Fragment API but unused.
-     * @param superInflater The result of the view's inflater, usually the result of super.getLayoutInflater()
-     * @return the layout inflater to use
-     */
     @CheckResult
     public LayoutInflater getLayoutInflater(Bundle savedInstanceState, LayoutInflater superInflater) {
-        if (!showAsSlidingFragment) {
+        if (!mShowSliding) {
             return superInflater;
         }
-        slidingUpLayout = getSlidingUpLayout();
-        if (slidingUpLayout != null) {
-            return LayoutInflater.from(slidingUpLayout.getContext());
+        mSlidingUpLayout = getSlidingUpLayout();
+        if (mSlidingUpLayout != null) {
+            return LayoutInflater.from(mSlidingUpLayout.getContext());
         }
-        return LayoutInflater.from(fragment.getContext());
+        return LayoutInflater.from(mFragment.getContext());
     }
 
     public void onAttach(Context context) {
         if(DEBUG) Log.d(TAG, "-----onAttach()");
     }
 
-    public void onDetach() {
-        if(DEBUG) Log.d(TAG, "-----onDetach()");
-    }
-
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         if(DEBUG) Log.d(TAG, "-----onActivityCreated()");
+        mViewDestroyed = false;
 
-        if (!showAsSlidingFragment) {
+        if (!mShowSliding) {
             return;
         }
 
-        View view = fragment.getView();
-        if (view != null && view.getParent() != null) {
-                throw new IllegalStateException("SlidingUpFragment can not be attached to a container view");
+        View view = mFragment.getView();
+        if (view == null) {
+            return;
         }
 
-        if(slidingUpLayout != null) {
-            slidingUpLayout.addView(fragment.getView());
-            slidingUpLayout.addPaneListener(this);
-            if(savedInstanceState == null) {
-                slidingUpLayout.setState(getState(fragment, EXPANDED));
-            }
+        if(view.getParent() != null)
+            throw new IllegalStateException(mFragment + " can not be attached to a container view");
+
+        if(mSlidingUpLayout == null)
+            throw new IllegalStateException("can't find SlidingUpPaneLayout with id "+mSlidingPaneLayoutId);
+
+        mSlidingUpLayout.addView(view);
+        mSlidingUpLayout.addPaneListener(this);
+
+        if(savedInstanceState == null) {
+            mSlidingUpLayout.setState(getPanelState(EXPANDED));
         }
     }
 
     public void onStart() {
         if(DEBUG) Log.d(TAG, "-----onStart()");
-        viewDestroyed = false;
     }
 
     public void onSaveInstanceState(Bundle outState) {
         if(DEBUG) Log.d(TAG, "-----onSaveInstanceState()");
-        if (!showAsSlidingFragment) {
+        if (!mShowSliding) {
             outState.putBoolean(SAVED_SHOWN_AS_SLIDING_FRAGMENT, false);
         }
-        if (backStackId != -1) {
-            outState.putInt(SAVED_BACK_STACK_ID, backStackId);
+        if (mBackStackId != -1) {
+            outState.putInt(SAVED_BACK_STACK_ID, mBackStackId);
         }
-        if (slidingPaneLayoutId != View.NO_ID) {
-            outState.putInt(SAVED_SLIDING_PANE_LAYOUT_ID, slidingPaneLayoutId);
+        if (mSlidingPaneLayoutId != View.NO_ID) {
+            outState.putInt(SAVED_SLIDING_PANE_LAYOUT_ID, mSlidingPaneLayoutId);
         }
     }
 
     public void onDestroyView() {
-        if(DEBUG) Log.d(TAG, "-----onDestroyView("+fragment.getView()+")");
-        viewDestroyed = true;
-        if(slidingUpLayout != null) {
-            slidingUpLayout.setState(HIDDEN);
-        }
+        if(DEBUG) Log.d(TAG, "-----onDestroyView("+mFragment.getView()+")");
+        mViewDestroyed = true;
+        onPanelHidden(mFragment.getView());
+    }
+
+    public void onDetach() {
+        if(DEBUG) Log.d(TAG, "-----onDetach()");
     }
 
     @Override
     @CallSuper
     public void onPanelHidden(View view) {
         if(DEBUG) Log.d(TAG, "-----onPanelHidden("+view+")");
-        slidingUpLayout.removeView(view);
-        slidingUpLayout.removePaneListener(this);
-        if(!viewDestroyed) dismissInternal(true);
+        if(mSlidingUpLayout != null) {
+            mSlidingUpLayout.removePaneListener(this);
+            mSlidingUpLayout.removeView(view);
+            if (!mViewDestroyed) dismissInternal(true);
+        }
     }
 
     private SlidingUpPaneLayout getSlidingUpLayout() {
-        if (slidingUpLayout == null) slidingUpLayout = findSlidingUpLayout();
-        return slidingUpLayout;
+        if (mSlidingUpLayout == null) mSlidingUpLayout = findSlidingUpLayout();
+        return mSlidingUpLayout;
     }
 
     @Nullable
     private SlidingUpPaneLayout findSlidingUpLayout() {
-        Fragment parentFragment = fragment.getParentFragment();
+        Fragment parentFragment = mFragment.getParentFragment();
         if (parentFragment != null) {
             View view = parentFragment.getView();
             if (view != null) {
-                return (SlidingUpPaneLayout) view.findViewById(slidingPaneLayoutId);
+                return (SlidingUpPaneLayout) view.findViewById(mSlidingPaneLayoutId);
             } else {
                 return null;
             }
         }
-        Activity parentActivity = fragment.getActivity();
+        Activity parentActivity = mFragment.getActivity();
         if (parentActivity != null) {
-            return (SlidingUpPaneLayout) parentActivity.findViewById(slidingPaneLayoutId);
+            return (SlidingUpPaneLayout) parentActivity.findViewById(mSlidingPaneLayoutId);
         }
         return null;
     }
 
-    static private Fragment setState(Fragment fragment, State state) {
+    static private Fragment putArg(Fragment fragment, String key, Serializable value) {
         Bundle args = fragment.getArguments();
         if(args == null) args = new Bundle();
-        args.putSerializable(ARG_PANEL_STATE, state);
+        args.putSerializable(key, value);
         fragment.setArguments(args);
         return fragment;
     }
 
-    static private State getState(Fragment fragment, State def) {
-        Bundle args = fragment.getArguments();
+    private State getPanelState(State def) {
+        Bundle args = mFragment.getArguments();
         if(args == null) return def;
         State state = (State) args.getSerializable(ARG_PANEL_STATE);
         if(state == null) return def;
--- extras/src/main/java/com/mypopsy/slidinguppanelayout/extra/SlidingUpFragmentInterface.java
+++ extras/src/main/java/com/mypopsy/slidinguppanelayout/extra/SlidingUpFragmentInterface.java
@@ -13,27 +13,29 @@ import com.mypopsy.widget.SlidingUpPaneLayout;
 public interface SlidingUpFragmentInterface {
 
     /**
-     * Display the bottom sheet, adding the fragment to the given FragmentManager. This does
-     * <em>not</em> add the transaction to the back stack. When teh fragment is dismissed, a new
+     * Display the sliding panel, adding the fragment to the given FragmentManager. This does
+     * <em>not</em> add the transaction to the back stack. When the fragment is dismissed, a new
      * transaction will be executed  to remove it from the activity.
      *
-     * @param manager The FragmentManager this fragment will be added to.
-     * @param slidingUpLayoutId The bottom sheet layoutId in the parent view to attach the
+     * @param manager the FragmentManager this fragment will be added to.
+     * @param slidingUpLayoutId the SlidingUpPaneLayout layoutId in the parent view to attach the
      * fragment to.
+     * @param state the initial state
      */
     void show(FragmentManager manager, @IdRes int slidingUpLayoutId, SlidingUpPaneLayout.State state);
     /**
-     * Display the bottom sheet, adding the fragment using an excising transaction and then
+     * Display the sliding panel, adding the fragment using an excisting transaction and then
      * committing the transaction.
      *
-     * @param transaction An existing transaction in which to add the fragment.
-     * @param slidingUpLayoutId The bottom sheet layoutId in the parent view to attach the
+     * @param transaction an existing transaction in which to add the fragment.
+     * @param slidingUpLayoutId the SlidingUpPaneLayout layoutId in the parent view to attach the
+     * @param state the initial state
      * fragment to.
      */
     int show(FragmentTransaction transaction, @IdRes int slidingUpLayoutId, SlidingUpPaneLayout.State state);
 
     /**
-     * Dismiss the fragment and it's bottom sheet. If the fragment was added to the back stack, all
+     * Dismiss the fragment and the sliding panel. If the fragment was added to the back stack, all
      * back stack state up to and including this entry will be popped. Otherwise, a new transaction
      * will be committed to remove this fragment.
      */
